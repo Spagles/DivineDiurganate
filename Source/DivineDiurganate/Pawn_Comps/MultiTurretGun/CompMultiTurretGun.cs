5d8719679a890412f21292e828d0764b45c6aa10
Tourswen <565033799@qq.com>
2025-12-25 12:00:01 +0800

    11

---
+++ Source/DivineDiurganate/Pawn_Comps/MultiTurretGun/CompMultiTurretGun.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Verse;
+using Verse.AI;
+using RimWorld;
+using UnityEngine;
+
+namespace DivineDiurganate
+{
+    public class CompProperties_MultiTurretGun : CompProperties_TurretGun
+    {
+        public int ID;
+        public CompProperties_MultiTurretGun()
+        {
+            compClass = typeof(Comp_MultiTurretGun);
+        }
+    }
+    public class Comp_MultiTurretGun : CompTurretGun
+    {
+        private bool fireAtWill = true;
+        public new CompProperties_MultiTurretGun Props => (CompProperties_MultiTurretGun)props;
+        public override void CompTick()
+        {
+            base.CompTick();
+            if (!currentTarget.IsValid && burstCooldownTicksLeft <= 0)
+            {
+                // 在其他情况下没有目标且冷却结束时也回正
+                curRotation = parent.Rotation.AsAngle + Props.angleOffset;
+            }
+        }
+        private void MakeGun()
+        {
+            gun = ThingMaker.MakeThing(Props.turretDef);
+            UpdateGunVerbs();
+        }
+        private void UpdateGunVerbs()
+        {
+            List<Verb> allVerbs = gun.TryGetComp<CompEquippable>().AllVerbs;
+            for (int i = 0; i < allVerbs.Count; i++)
+            {
+                Verb verb = allVerbs[i];
+                verb.caster = parent;
+                verb.castCompleteCallback = delegate
+                {
+                    burstCooldownTicksLeft = AttackVerb.verbProps.defaultCooldownTime.SecondsToTicks();
+                };
+            }
+        }
+        public override void PostExposeData()
+        {
+            Scribe_Values.Look(ref burstCooldownTicksLeft, "burstCooldownTicksLeft", 0);
+            Scribe_Values.Look(ref burstWarmupTicksLeft, "burstWarmupTicksLeft", 0);
+            Scribe_TargetInfo.Look(ref currentTarget, "currentTarget_" + Props.ID);
+            Scribe_Deep.Look(ref gun, "gun_" + Props.ID);
+            Scribe_Values.Look(ref fireAtWill, "fireAtWill", defaultValue: true);
+            if (Scribe.mode == LoadSaveMode.PostLoadInit)
+            {
+                if (gun == null)
+                {
+                    MakeGun();
+                }
+                else
+                {
+                    UpdateGunVerbs();
+                }
+            }
+        }
+    }
+
+}
--- Source/DivineDiurganate/Work/ThinkNode_ConditionalMechHasPilot.cs
+++ Source/DivineDiurganate/Work/ThinkNode_ConditionalMechHasPilot.cs
@@ -1,4 +1,4 @@
-// ThinkNode_ConditionalMechHasPilot.cs
+// ThinkNode_ConditionalMechHasPilot.cs (修复版)
 using RimWorld;
 using Verse;
 using Verse.AI;
@@ -9,14 +9,24 @@ namespace DivineDiurganate
     {
         // 可选的：可以在XML中设置的参数
         public int minPilotCount = 1;  // 最少需要的驾驶员数量
-        
+
         protected override bool Satisfied(Pawn pawn)
         {
             var pilotComp = pawn.TryGetComp<CompMechPilotHolder>();
-            
-            return !pilotComp.HasPilots;
+            if (pilotComp == null)
+                return false; // 如果没有驾驶员组件，条件满足（允许执行）
+
+            int currentPilotCount = pilotComp.CurrentPilotCount;
+
+            // 检查是否满足最小驾驶员数量要求
+            bool hasEnoughPilots = currentPilotCount >= minPilotCount;
+
+            // 这意味着机甲可以正常工作
+            bool conditionSatisfied = hasEnoughPilots;
+
+            return !conditionSatisfied;
         }
-        
+
         public override ThinkNode DeepCopy(bool resolve = true)
         {
             ThinkNode_ConditionalMechHasPilot thinkNode_ConditionalMechHasPilot = (ThinkNode_ConditionalMechHasPilot)base.DeepCopy(resolve);
