ba9be624bcdfd446d626a7d06803b237694f06f9
Tourswen <82707327+Tourswen@users.noreply.github.com>
2026-01-18 23:20:52 +0800

    11

--- Source/DivineDiurganate/BuildingComps/TradeTerminal/CompProperties_TradeTerminal.cs
+++ Source/DivineDiurganate/BuildingComps/TradeTerminal/CompProperties_TradeTerminal.cs
@@ -7,7 +7,9 @@ namespace DivineDiurganate
         public float maxTradeRange = 0f;
         public bool requiresPower = true;
         public string gizmoIconPath = "UI/Commands/Trade";
+        [MustTranslate]
         public string gizmoLabel = "DD_GizmoTradeWithFlyoverLabel";
+        [MustTranslate]
         public string gizmoDesc = "DD_GizmoTradeWithFlyoverDesc";
 
         public CompProperties_TradeTerminal()
